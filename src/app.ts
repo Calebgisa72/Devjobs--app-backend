91ced415fe65e025e50a9b84fce67d67c1cd2982
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2024-04-20 12:36:13 +0200

    added welcome page

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 
@@ -7,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('Welcome To My Brand');
+})
 
 app.use('/api/auth', authRoutes);
 
